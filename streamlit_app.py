4100cc0ecf12436d1e89da25dc9d3e7e19ad1803
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-02-17 15:56:49 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,98 +18,98 @@ def process_prompt(input):
 
   if api_key:
 
-# Agregamos un título al principio
-st.title('Evaluador de ensayos')
+  # Agregamos un título al principio
+  st.title('Evaluador de ensayos')
 
-# Agregamos información de instrucciones
-st.write('Suba un archivo .XLSX con los ensayos de sus alumnos. Máximo: 10 ensayos.')
+  # Agregamos información de instrucciones
+  st.write('Suba un archivo .XLSX con los ensayos de sus alumnos. Máximo: 10 ensayos.')
 
-# Añadimos la columna de la izquierda con el título, descripción y caja de API Key
-st.sidebar.title('Herramientas de evaluación')
-st.sidebar.write('Ingrese su API Key de OpenAI para poder utilizar esta herramienta:')
-api_key = st.sidebar.text_input('API Key')
+  # Añadimos la columna de la izquierda con el título, descripción y caja de API Key
+  st.sidebar.title('Herramientas de evaluación')
+  st.sidebar.write('Ingrese su API Key de OpenAI para poder utilizar esta herramienta:')
+  api_key = st.sidebar.text_input('API Key')
 
-# Verificar si se ingresó una API Key válida
-if api_key:
-    try:
-        # Activar la API Key de OpenAI
-        openai.api_key = api_key
+  # Verificar si se ingresó una API Key válida
+  if api_key:
+      try:
+          # Activar la API Key de OpenAI
+          openai.api_key = api_key
 
-        # Crear una variable de estado para verificar si el usuario ya ha cargado un archivo
-        if 'archivo_cargado' not in st.session_state:
-            st.session_state.archivo_cargado = False
+          # Crear una variable de estado para verificar si el usuario ya ha cargado un archivo
+          if 'archivo_cargado' not in st.session_state:
+              st.session_state.archivo_cargado = False
 
-        # Pedimos al usuario que suba el archivo Excel
-        if not st.session_state.archivo_cargado:
-            archivo = st.file_uploader('Cargar archivo Excel', type=['xlsx'])
+          # Pedimos al usuario que suba el archivo Excel
+          if not st.session_state.archivo_cargado:
+              archivo = st.file_uploader('Cargar archivo Excel', type=['xlsx'])
 
-            if archivo:
-                # Leemos el archivo con pandas
-                data = pd.read_excel(archivo)
+              if archivo:
+                  # Leemos el archivo con pandas
+                  data = pd.read_excel(archivo)
 
-                # Pedimos al usuario que seleccione las columnas con el título y el ensayo
-                columnas = data.columns
-                columna_titulo = st.selectbox('Selecciona la columna que contiene los títulos:', columnas)
-                columna_ensayo = st.selectbox('Selecciona la columna que contiene los ensayos:', columnas)
+                  # Pedimos al usuario que seleccione las columnas con el título y el ensayo
+                  columnas = data.columns
+                  columna_titulo = st.selectbox('Selecciona la columna que contiene los títulos:', columnas)
+                  columna_ensayo = st.selectbox('Selecciona la columna que contiene los ensayos:', columnas)
 
-                # Agregamos un botón para iniciar la evaluación
-                if st.button('Evaluar'):
-                    # Obtenemos los títulos y los ensayos del archivo
-                    titulos = data[columna_titulo].head(10).tolist()
-                    ensayos = data[columna_ensayo].head(10).tolist()
+                  # Agregamos un botón para iniciar la evaluación
+                  if st.button('Evaluar'):
+                      # Obtenemos los títulos y los ensayos del archivo
+                      titulos = data[columna_titulo].head(10).tolist()
+                      ensayos = data[columna_ensayo].head(10).tolist()
 
-                    # Utilizamos la API de GPT-3 para calificar cada ensayo
-                    resultados = []
-                    for i, ensayo in enumerate(ensayos):
-                        prompt = f"Califica el ensayo titulado '{titulos[i]}'. "
-                        prompt += f"Ensayo: {ensayo}. "
-                        response = openai.Completion.create(
-                            engine="text-davinci-003",
-                            prompt=prompt,
-                            temperature=0,
-                            max_tokens=512,
-                            n=1,
-                            stop=None
-                        )
-                        justificacion = response.choices[0].text.strip()
+                      # Utilizamos la API de GPT-3 para calificar cada ensayo
+                      resultados = []
+                      for i, ensayo in enumerate(ensayos):
+                          prompt = f"Califica el ensayo titulado '{titulos[i]}'. "
+                          prompt += f"Ensayo: {ensayo}. "
+                          response = openai.Completion.create(
+                              engine="text-davinci-003",
+                              prompt=prompt,
+                              temperature=0,
+                              max_tokens=512,
+                              n=1,
+                              stop=None
+                          )
+                          justificacion = response.choices[0].text.strip()
 
-                        # Agregamos sugerencias de mejora a la justificación
-                        response = openai.Completion.create(
-                            engine="text-davinci-003",
-                            prompt=f"Sugiere mejoras para el ensayo titulado '{titulos[i]}'. Ensayo: {ensayo}",
-                            temperature=0,
-                            max_tokens=512,
-                            n=1,
-                            stop=None,
-                            timeout=60,
-                        )
-                        sugerencias = response.choices[0].text.strip()
+                          # Agregamos sugerencias de mejora a la justificación
+                          response = openai.Completion.create(
+                              engine="text-davinci-003",
+                              prompt=f"Sugiere mejoras para el ensayo titulado '{titulos[i]}'. Ensayo: {ensayo}",
+                              temperature=0,
+                              max_tokens=512,
+                              n=1,
+                              stop=None,
+                              timeout=60,
+                          )
+                          sugerencias = response.choices[0].text.strip()
 
-                        # Agregamos la calificación y las sugerencias de mejora a la tabla
-                        resultados.append({
-                            'Ensayo': titulos[i],
-                            'Justificación': justificacion,
-                            'Sugerencias de mejora': sugerencias,
-                    })
+                          # Agregamos la calificación y las sugerencias de mejora a la tabla
+                          resultados.append({
+                              'Ensayo': titulos[i],
+                              'Justificación': justificacion,
+                              'Sugerencias de mejora': sugerencias,
+                      })
 
-                # Mostramos los resultados en una tabla
-                st.write('Resultados:')
-                tabla = pd.DataFrame(resultados)
-                st.table(tabla)
+                  # Mostramos los resultados en una tabla
+                  st.write('Resultados:')
+                  tabla = pd.DataFrame(resultados)
+                  st.table(tabla)
 
-                # Guardar los resultados en un archivo de Excel
-                nombre_archivo = 'resultados.xlsx'
-                tabla.to_excel(nombre_archivo, index=False)
+                  # Guardar los resultados en un archivo de Excel
+                  nombre_archivo = 'resultados.xlsx'
+                  tabla.to_excel(nombre_archivo, index=False)
 
-                # Descargar el archivo de Excel
-                st.download_button(
-                    label='Descargar resultados en Excel',
-                    data=open(nombre_archivo, 'rb').read(),
-                    file_name=nombre_archivo,
-                    mime='application/vnd.ms-excel'
-                )
+                  # Descargar el archivo de Excel
+                  st.download_button(
+                      label='Descargar resultados en Excel',
+                      data=open(nombre_archivo, 'rb').read(),
+                      file_name=nombre_archivo,
+                      mime='application/vnd.ms-excel'
+                  )
 
-                # Actualizamos la variable de estado del archivo cargado
-                st.session_state.archivo_cargado = True
+                  # Actualizamos la variable de estado del archivo cargado
+                  st.session_state.archivo_cargado = True
 
-                st.sidebar.write('Acceda a más herramientas en https://razonamientocritico.online/')
+                  st.sidebar.write('Acceda a más herramientas en https://razonamientocritico.online/')

4e77b844b1ab04633cb8dab564e72ff2f7e7d5f0
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-02-17 07:48:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,13 +9,12 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 # Agregamos un título al principio
 st.title('Evaluador de ensayos')
 
-# Agregamos información en el cuerpo
-st.write('Suba un archivo .XLSX con los ensayos de sus alumnos. Seleccione la columna que contiene los títulos y la que contiene los ensayos, y presione el botón "Evaluar".')
+# Agregamos información de instrucciones
+st.write('Suba un archivo .XLSX con los ensayos de sus alumnos.')
 
 # Pedimos al usuario que suba el archivo Excel
 archivo = st.file_uploader('Cargar archivo Excel', type=['xlsx'])
 
-# Si se subió un archivo, lo procesamos
 if archivo:
     # Leemos el archivo con pandas
     data = pd.read_excel(archivo)
@@ -25,46 +24,48 @@ if archivo:
     columna_titulo = st.selectbox('Selecciona la columna que contiene los títulos:', columnas)
     columna_ensayo = st.selectbox('Selecciona la columna que contiene los ensayos:', columnas)
 
-    # Obtenemos los títulos y los ensayos del archivo
-    titulos = data[columna_titulo].tolist()
-    ensayos = data[columna_ensayo].tolist()
+    # Agregamos un botón para iniciar la evaluación
+    if st.button('Evaluar'):
+        # Obtenemos los títulos y los ensayos del archivo
+        titulos = data[columna_titulo].tolist()
+        ensayos = data[columna_ensayo].tolist()
 
-    # Utilizamos la API de GPT-3 para calificar cada ensayo
-    resultados = []
-    for i, ensayo in enumerate(ensayos):
-        prompt = f"Califica el ensayo titulado '{titulos[i]}'. "
-        prompt += f"Ensayo: {ensayo}. "
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            temperature=0.5,
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            timeout=60,
-        )
-        justificacion = response.choices[0].text.strip()
+        # Utilizamos la API de GPT-3 para calificar cada ensayo
+        resultados = []
+        for i, ensayo in enumerate(ensayos):
+            prompt = f"Califica el ensayo titulado '{titulos[i]}'. "
+            prompt += f"Ensayo: {ensayo}. "
+            response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=prompt,
+                temperature=0.5,
+                max_tokens=1024,
+                n=1,
+                stop=None,
+                timeout=60,
+            )
+            justificacion = response.choices[0].text.strip()
 
-        # Agregamos sugerencias de mejora a la justificación
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=f"Sugiere mejoras para el ensayo titulado '{titulos[i]}'. Ensayo: {ensayo}",
-            temperature=0,
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            timeout=60,
-        )
-        sugerencias = response.choices[0].text.strip()
+            # Agregamos sugerencias de mejora a la justificación
+            response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=f"Sugiere mejoras para el ensayo titulado '{titulos[i]}'. Ensayo: {ensayo}",
+                temperature=0,
+                max_tokens=1024,
+                n=1,
+                stop=None,
+                timeout=60,
+            )
+            sugerencias = response.choices[0].text.strip()
 
-        # Agregamos la calificación y las sugerencias de mejora a la tabla
-        resultados.append({
-            'Ensayo': titulos[i],
-            'Justificación': justificacion,
-            'Sugerencias de mejora': sugerencias,
-        })
+            # Agregamos la calificación y las sugerencias de mejora a la tabla
+            resultados.append({
+                'Ensayo': titulos[i],
+                'Justificación': justificacion,
+                'Sugerencias de mejora': sugerencias,
+            })
 
-    # Mostramos los resultados en una tabla
-    st.write('Resultados:')
-    tabla = pd.DataFrame(resultados, columns=['Ensayo', 'Justificación', 'Sugerencias de mejora'])
-    st.table(tabla)
+        # Mostramos los resultados en una tabla
+        st.write('Resultados:')
+        tabla = pd.DataFrame(resultados)
+        st.table(tabla)

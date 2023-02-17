5382414393630bc7db70438eb46c6da6979c4d9d
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-02-17 06:31:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,15 +9,15 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 # Agregamos información en una columna a la izquierda
 st.sidebar.title('Calificador de ensayos')
 st.sidebar.subheader('Instrucciones')
-st.sidebar.markdown('Suba un archivo .XLSX con los ensayos de sus alumnos y especifique qué columna contiene los ensayos. Defina el criterio global de calificación.')
+st.sidebar.markdown('Suba un archivo .XLSX con los ensayos de sus alumnos y especifique qué columna contiene los ensayos. Defina los criterios de calificación.')
 st.sidebar.subheader('Autor')
 st.sidebar.markdown('Moris Polanco')
 
 # Pedimos al usuario que suba el archivo Excel
 archivo = st.file_uploader('Cargar archivo Excel', type=['xlsx'])
 
-# Pedimos al usuario que defina el criterio global de calificación
-criterio_global = st.slider('¿Qué tan buena es la argumentación en general?', min_value=1, max_value=10, step=1)
+# Definimos los criterios de evaluación
+criterios = ['Ortografía', 'Argumentación']
 
 # Si se subió un archivo, lo procesamos
 if archivo:
@@ -28,12 +28,18 @@ if archivo:
     columna_ensayos = st.selectbox('Selecciona la columna que contiene los ensayos:', data.columns)
     ensayos = data[columna_ensayos].tolist()
 
+    # Pedimos al usuario que valore cada criterio
+    valores_criterios = {}
+    for criterio in criterios:
+        valor = st.slider(f'¿Qué tan importante es el criterio {criterio}?', min_value=1, max_value=10, step=1)
+        valores_criterios[criterio] = valor
+
     # Utilizamos la API de GPT-3 para calificar cada ensayo
     resultados = []
     for ensayo in ensayos:
-        prompt = f"Califica este ensayo. Argumentación en general: {criterio_global}. Ensayo: {ensayo}."
+        prompt = f"Califica este ensayo. Ortografía: {valores_criterios['Ortografía']}. Argumentación: {valores_criterios['Argumentación']}. Ensayo: {ensayo}."
         response = openai.Completion.create(
-            engine="text-davinci-002",
+            engine="text-davinci-003",
             prompt=prompt,
             temperature=0.5,
             max_tokens=1024,
@@ -42,7 +48,10 @@ if archivo:
             timeout=60,
         )
         justificacion = response.choices[0].text.strip()
-        resultados.append({'Ensayo': ensayo, 'Calificación': justificacion})
+        nota_ortografia = int(justificacion.split("Ortografía:")[1].split(".")[0].strip())
+        nota_argumentacion = int(justificacion.split("Argumentación:")[1].split(".")[0].strip())
+        nota_total = nota_ortografia * valores_criterios['Ortografía'] + nota_argumentacion * valores_criterios['Argumentación']
+        resultados.append({'Calificación': nota_total, 'Justificación': justificacion})
 
     # Mostramos los resultados en una tabla
     st.write('Resultados:')

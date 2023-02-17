a953e248cdc6415c206f48f14cba7cbed40c090a
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-02-17 07:19:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ if archivo:
         prompt = f"Califica el ensayo titulado '{titulos[i]}'. "
         prompt += f"Ensayo: {ensayo}. "
         response = openai.Completion.create(
-            engine="text-davinci-002",
+            engine="text-davinci-003",
             prompt=prompt,
             temperature=0.5,
             max_tokens=1024,

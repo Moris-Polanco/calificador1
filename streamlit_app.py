db8f5eab35e76d630d000ab5f758788d5638e2ec
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-02-17 11:27:26 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,32 +79,33 @@ if archivo:
         if st.button('Exportar resultados a Word'):
             st.write('Exportando resultados...')
             with st.spinner('Exportando resultados...'):
-        # Creamos un nuevo documento Word
+                
+            # Creamos un nuevo documento Word
             document = Document()
 
-        # Agregamos un encabezado
-        document.add_heading('Resultados de la evaluación', 0)
+            # Agregamos un encabezado
+            document.add_heading('Resultados de la evaluación', 0)
 
-        # Agregamos la tabla de resultados
-        table = document.add_table(rows=len(resultados)+1, cols=3)
-        table.style = 'Table Grid'
-        hdr_cells = table.rows[0].cells
-        hdr_cells[0].text = 'Ensayo'
-        hdr_cells[1].text = 'Justificación'
-        hdr_cells[2].text = 'Sugerencias de mejora'
-        for i, resultado in enumerate(resultados):
-            row_cells = table.rows[i+1].cells
-            row_cells[0].text = resultado['Ensayo']
-            row_cells[1].text = resultado['Justificación']
-            row_cells[2].text = resultado['Sugerencias de mejora']
+            # Agregamos la tabla de resultados
+            table = document.add_table(rows=len(resultados)+1, cols=3)
+            table.style = 'Table Grid'
+            hdr_cells = table.rows[0].cells
+            hdr_cells[0].text = 'Ensayo'
+            hdr_cells[1].text = 'Justificación'
+            hdr_cells[2].text = 'Sugerencias de mejora'
+            for i, resultado in enumerate(resultados):
+                row_cells = table.rows[i+1].cells
+                row_cells[0].text = resultado['Ensayo']
+                row_cells[1].text = resultado['Justificación']
+                row_cells[2].text = resultado['Sugerencias de mejora']
 
-        # Guardamos el documento en un archivo
-        nombre_archivo = 'resultados.docx'
-        document.save(nombre_archivo)
+            # Guardamos el documento en un archivo
+            nombre_archivo = 'resultados.docx'
+            document.save(nombre_archivo)
 
-        # Descargamos el archivo Word a la carpeta de descargas del usuario
-        from google.colab import files
-        files.download(nombre_archivo, root_dir='/content/downloads')
+            # Descargamos el archivo Word a la carpeta de descargas del usuario
+            from google.colab import files
+            files.download(nombre_archivo, root_dir='/content/downloads')
 
-        st.success(f'Los resultados han sido exportados al archivo {nombre_archivo}')
+            st.success(f'Los resultados han sido exportados al archivo {nombre_archivo}')
 

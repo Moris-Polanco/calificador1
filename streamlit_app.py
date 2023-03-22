d7a23b0b6ead72e3890e6333a092516d8b513c47
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-03-22 14:55:32 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,4 @@
 import streamlit as st
-
-hide_menu_style = """
-        <style>
-        #MainMenu {visibility: hidden;}
-        </style>
-        """
-st.markdown(hide_menu_style, unsafe_allow_html=True)
-
 import pandas as pd
 import openai
 import os
